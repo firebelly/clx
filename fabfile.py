3a6e4d098cea991117f1d8b77165661b43f10711
Nate Beaty <nate@firebellydesign.com>
2018-06-26 17:35:12 -0500

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,18 @@
 from fabric.api import *
 import os
 
-env.hosts = ['aei-craft.firebelly.co']
-env.user = 'firebelly'
-env.path = '~/Sites/aei-craft'
-env.remotepath = '/home/firebelly/webapps/aei'
+env.hosts = ['staging.chicagolx.org']
+env.user = 'chicagolx'
+env.path = '~/Sites/clx'
+env.remotepath = '/home/chicagolx/webapps/clx_staging'
 env.git_branch = 'master'
 env.warn_only = True
 env.remote_protocol = 'http'
 
 def production():
-  env.hosts = ['aeieng.com']
-  env.user = 'deployer'
-  env.remotepath = '/var/www/aei-craft/'
+  env.hosts = ['chicagolx.org']
+  env.user = 'chicagolx'
+  env.remotepath = '/home/chicagolx/webapps/clx_production'
   env.git_branch = 'master'
   env.remote_protocol = 'https'
 
@@ -23,7 +23,7 @@ def devsetup():
   print "Installing composer, node and bower assets...\n"
   local('composer install')
   local('npm install')
-  local('cd public/assets && bower install')
+  local('cd assets && bower install')
   local('npx gulp')
   local('cp .env-example .env')
   print "OK DONE! Hello? Are you still awake?\nEdit your .env file with local credentials\nRun `npx gulp watch` to run local gulp to compile & watch assets"
