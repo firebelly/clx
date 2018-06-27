ae4699ae91ed378fa24184bf0f051d3dc31233f9
Nate Beaty <nate@firebellydesign.com>
2018-06-27 12:34:17 -0500

    add jshint to gulpfile, cleaner cli options, add autoprefixer options, simplify build sequence

--- gulpfile.js
+++ gulpfile.js
@@ -16,48 +16,73 @@ var svgstore     = require('gulp-svgstore');
 var svgmin       = require('gulp-svgmin');
 var rename       = require('gulp-rename');
 var argv         = require('minimist')(process.argv.slice(2));
-var isProduction = argv.production;
+var jshint       = require('gulp-jshint');
 
-// smash CSS!
+// CLI options
+var enabled = {
+  // Enable static asset revisioning when `--production`
+  rev: argv.production,
+  // Disable source maps when `--production`
+  maps: !argv.production,
+  // Fail due to JSHint warnings only when `--production`
+  failJSHint: argv.production,
+  // Strip debug statments from javascript when `--production`
+  stripJSDebug: argv.production
+};
+
+// Smash CSS!
 gulp.task('styles', function() {
   return gulp.src([
       'assets/scss/main.scss'
     ])
-    .pipe(gulpif(!isProduction, sourcemaps.init()))
+    .pipe(gulpif(enabled.maps, sourcemaps.init()))
     .pipe(sass())
-    .on('error', notify.onError(function (error) {
+    .on('error', notify.onError(function(error) {
        return 'Styles error!' + error;
     }))
-    .pipe(autoprefixer())
-    .pipe(gulpif(isProduction, cssnano()))
+    .pipe(autoprefixer({
+      browsers: [
+        'last 2 versions',
+        'android 4',
+        'opera 12'
+      ]
+    }))
+    .pipe(cssnano({
+      safe: true
+    }))
     .pipe(gulp.dest('web/assets/dist/css'))
-    .pipe(gulpif(!isProduction, sourcemaps.write('maps')))
-    .pipe(gulpif(!isProduction, gulp.dest('web/assets/dist/css')))
+    .pipe(gulpif(enabled.maps, sourcemaps.write('maps')))
+    .pipe(gulpif(enabled.maps, gulp.dest('web/assets/dist/css')))
     .pipe(browserSync.stream({match: '**/*.css'}))
     .pipe(notify({message: 'Styles smashed.', onLast: true}));
 });
 
-// smash javascript!
-gulp.task('scripts', function() {
+// Smash javascript!
+gulp.task('scripts', ['jshint'], function() {
   return gulp.src([
       'assets/js/main.js'
     ])
     .pipe(include())
     .pipe(concat('main.js'))
-    .pipe(gulpif(!isProduction, sourcemaps.init()))
-    .pipe(uglify())
-    .on('error', notify.onError(function (error) {
+    .pipe(gulpif(enabled.maps, sourcemaps.init()))
+    .pipe(uglify({
+      compress: {
+        'drop_debugger': enabled.stripJSDebug
+      }
+    }))
+    .on('error', notify.onError(function(error) {
        return 'Styles error!' + error;
     }))
     .pipe(gulp.dest('web/assets/dist/js'))
-    .pipe(gulpif(!isProduction, sourcemaps.write('maps')))
-    .pipe(gulpif(!isProduction, gulp.dest('web/assets/dist/js')))
+    .pipe(gulpif(enabled.maps, sourcemaps.write('maps')))
+    .pipe(gulpif(enabled.maps, gulp.dest('web/assets/dist/js')))
     .pipe(browserSync.reload({stream:true}))
     .pipe(notify({message: 'Scripts smashed.', onLast: true}));
 });
 
-// revision files for production assets
+// Revision files for production assets
 gulp.task('rev', function() {
+  if (!enabled.rev) return;
   return gulp.src(['web/assets/dist/**/*.{css,js,jpg,png,svg,gif}'])
     .pipe(rev())
     .pipe(gulp.dest('web/assets/dist'))
@@ -65,15 +90,15 @@ gulp.task('rev', function() {
     .pipe(gulp.dest('web/assets/dist'));
 });
 
-// copy various files/dirs to dist
+// Copy various files/dirs to dist
 gulp.task('copy', function() {
   gulp.src(['assets/js/modernizr.custom.js'])
-    .pipe(gulp.dest('web/assets/dist/js/'))
+    .pipe(gulp.dest('web/assets/dist/js/'));
   return gulp.src(['assets/images/*'])
-    .pipe(gulp.dest('web/assets/dist/images/'))
+    .pipe(gulp.dest('web/assets/dist/images/'));
 });
 
-// folders to watch for changes
+// Folders to watch for changes
 gulp.task('watch', ['build'], function() {
   // Init BrowserSync
   browserSync.init({
@@ -81,9 +106,18 @@ gulp.task('watch', ['build'], function() {
     notify: false,
     open: false
   });
-
   gulp.watch('assets/scss/**/*.scss', ['styles']);
   gulp.watch('assets/js/**/*.js', ['scripts']);
+});
+
+// `gulp jshint` - Lints configuration JSON and project JS.
+gulp.task('jshint', function() {
+  return gulp.src([
+      'assets/bower.json', 'gulpfile.js', 'assets/js/main.js'
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(gulpif(enabled.failJSHint, jshint.reporter('fail')));
 });
 
 // `gulp clean` - Deletes the build folder entirely.
@@ -91,22 +125,12 @@ gulp.task('clean', require('del').bind(null, ['web/assets/dist']));
 
 // `gulp build` - Run all the build tasks but don't clean up beforehand.
 gulp.task('build', function(callback) {
-  if (isProduction) {
-    // production gulpin' (with file revisioning)
-    runSequence(
-      'clean',
-      ['copy', 'styles', 'scripts', 'svgs'],
-      'rev',
-      callback
-    );
-  } else {
-    // dev gulpin'
-    runSequence(
-      'clean',
-      ['copy', 'styles', 'scripts', 'svgs'],
-      callback
-    );
-  }
+  runSequence(
+    'clean',
+    ['copy', 'styles', 'scripts', 'svgs'],
+    'rev',
+    callback
+  );
 });
 
 // SVGs to defs
@@ -124,7 +148,7 @@ gulp.task('svgs', function() {
         }]
     }))
     .pipe(gulp.dest('web/assets/dist/svgs/'))
-    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(svgstore({inlineSvg: true}))
     .pipe(rename({suffix: '-defs'}))
     .pipe(gulp.dest('web/assets/dist/'));
 });
